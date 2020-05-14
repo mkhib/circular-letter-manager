afd13138bfd77301ef779d6a48ead21203d19385
mkhib <mkhib@ymail.com>
2020-05-14 16:56:08 +0430

    check

--- front-end/website/src/index.tsx
+++ front-end/website/src/index.tsx
@@ -38,7 +38,7 @@ import { setContext } from 'apollo-link-context';
 // }
 
 const uploadLink = createUploadLink({
-  uri: "https://20e96dd7.ngrok.io/graphql",
+  uri: "https://2485f4e1.ngrok.io/graphql",
   credentials: 'include'
 });
 
